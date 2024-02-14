0583af2203d4a7a2b311bdafabc02d0299756c28
ahmadaarif <ahmadarif153@gmail.com>
2024-02-14 19:35:04 +0500

    Trying the code with activated venv

--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
-python manage.py collectstatic && gunicorn --workers 2 expenseWeb.wsgi
+# python manage.py collectstatic && gunicorn --workers 2 expenseWeb.wsgi
+python manage.py collectstatic --noinput && gunicorn --workers 2 expenseWeb.wsgi:application
